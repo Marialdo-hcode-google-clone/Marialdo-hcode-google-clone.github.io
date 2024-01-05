69ecdedc81cb41688e6dd5b715a156fadf4833ce
Marialdo Ferreira <marialdoferreira@gmail.com>
2024-01-04 22:50:22 -0300

    Inicial

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+    console.log("Clicou aqui")
+}
+)
+
+const botaoEstouComSorte = document.querySelector(`form [type="button"]`)
+
+botaoEstouComSorte.addEventListener("click", () => {
+    location.href = "https://class.hcode.com.br"
+})
